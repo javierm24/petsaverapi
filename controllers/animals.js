a414af138140eef26249616a2d58cc5e2d44d962
Javier Machado Vieira <javierm1694@gmail.com>
2019-07-20 18:09:24 +0200

    agregado try y catch

--- controllers/animals.js
+++ controllers/animals.js
@@ -3,29 +3,39 @@ const router = express.Router();
 const animalModel = require("../models/animal-model");
 
 router.post('/animales', (req, res) => {
-
-    let animal = new animalModel(
-        {
-            imagen: req.body.imagen,
-            tipo: req.body.tipo,
-            nombre: req.body.nombre,
-            edad: req.body.edad,
-            descripcion: req.body.descripcion,
-            dueno: mongoose.Types.ObjectId(req.body.dueno)
-        });
-    animal.save()
-    res.send("todo ok")
+    try {
+        let animal = new animalModel(
+            {
+                imagen: req.body.imagen,
+                tipo: req.body.tipo,
+                nombre: req.body.nombre,
+                edad: req.body.edad,
+                descripcion: req.body.descripcion,
+                dueno: mongoose.Types.ObjectId(req.body.dueno)
+            });
+        animal.save()
+        res.send("todo ok")
+    } catch (err) {
+        res.status(500).json({
+            error: "Database error"
+        })
+    }
 })
 
 router.get('/animales/search/:tipo', async (req, res) => {
-    let result = await animalModel.find({ tipo: req.params.tipo })
-    res.send(result)
+    try {
+        let result = await animalModel.find({ tipo: req.params.tipo })
+        res.send(result)
+    } catch (err) {
+        res.status(500).json({
+            error: "Database error"
+        })
+    }
 })
 
-router.delete('/animales/:id', function (req, res) {
-    animalModel.findByIdAndDelete({ _id: req.params.id }).then(function () {
-        res.send('ELIMINADO')
-    })
+router.delete('/animales/:id', async (req, res) => {
+    await animalModel.findByIdAndDelete({ _id: req.params.id })
+    res.send('ELIMINADO')
 })
 
 router.get('/animales/:dueno', async (req, res) => {
