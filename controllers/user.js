99e146edbf5959206d8a2445307596747fbfead1
Javier Machado Vieira <javierm1694@gmail.com>
2019-07-27 18:12:18 +0200

    Enviar codigo 200 si el user esta correcto

--- controllers/user.js
+++ controllers/user.js
@@ -24,22 +24,29 @@ router.post('/usuario/login', async (req, res) => {
     try {
         const contrasena = req.body.contrasena;
         let user = await userModel.findOne({ usuario: req.body.usuario });
-        const hash = user.contrasena;
-        bcrypt.compare(contrasena, hash, (err, comp) => {
-            if (err) {
-                res.status(401).json({
-                    error: "Error. No estas autorizado"
-                })
-            } else {
-                if (comp) {
-                    res.send("todo ok");
-                } else {
+        if(!user){
+            res.status(401).json({
+                error: "No estas autorizado"
+            })
+        }else{
+            const hash = user.contrasena;
+            bcrypt.compare(contrasena, hash, (err, comp) => {
+                if (err) {
                     res.status(401).json({
-                        error: "No estas autorizado"
+                        error: "Error. No estas autorizado"
                     })
+                } else {
+                    if (comp) {
+                        res.status(200).json("todo ok");
+                    } else {
+                        res.status(401).json({
+                            error: "No estas autorizado"
+                        })
+                    }
                 }
-            }
-        })
+            })
+        }
+     
     } catch (error) {
         res.status(500).json({
             error: "Database error"
